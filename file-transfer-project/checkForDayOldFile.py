0a98239b8e36ca6f724abd47c8997f2f461608cf
Ethan <112027599+ethantl-1511@users.noreply.github.com>
2022-11-18 14:08:12 -0600

    Update checkForDayOldFile.py

--- file-transfer-project/checkForDayOldFile.py
+++ file-transfer-project/checkForDayOldFile.py
@@ -1,3 +1,9 @@
+# Python:   3.10.7
+#
+# Author:   Ethan LaRocca
+#
+# Purpose:  Snippet of code I created used for a function in a file transfer python project.
+
 import os
 from datetime import datetime, timedelta
 
@@ -12,5 +18,5 @@ for file in dir:
         expiredFileTime = fileTime + timedelta(hours = 24) # the file will be considered old after 24 hours
         if curTime <= expiredFileTime: # if the current time is less than or equal to the expiredFileTime, transfer files.
             print("The file is less than or equal to EXACTLY 24 hours old. Automatically Transfer.")
-        #else:
-        #    print("The file greater than 24 hours old. Do NOT Automatically Transfer.")
+        else:
+            print("The file greater than 24 hours old. Do NOT Automatically Transfer.")
